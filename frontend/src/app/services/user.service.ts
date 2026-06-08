265ec7530b5d0e32ae9757371f080b40f61ab3f7
pratik <pratikkanade007@gmail.com>
2026-06-08 12:21:47 +0530

    error solve

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -10,13 +10,15 @@ export class UserService {
   users$ = this.usersSubject.asObservable();
   private loadingSubject = new BehaviorSubject<boolean>(false);
   loading$ = this.loadingSubject.asObservable();
-  private api = 'http://localhost:3000/api/users';
+  private api = '/api';
+  // backend exposes user routes at /api/users
+  private usersApi = 'http://localhost:3000/api/users';
 
   constructor(private http: HttpClient) {}
 
   loadInitial(){
     this.loadingSubject.next(true);
-    this.http.get<User[]>(this.api).subscribe(users => {
+    this.http.get<User[]>(this.usersApi).subscribe(users => {
       this.usersSubject.next(users);
       this.loadingSubject.next(false);
     }, () => this.loadingSubject.next(false));
@@ -24,7 +26,7 @@ export class UserService {
 
   addUser(user: User): Observable<User>{
     this.loadingSubject.next(true);
-    return this.http.post<User>(this.api, user).pipe(tap(added => {
+    return this.http.post<User>(this.usersApi, user).pipe(tap(added => {
       const current = this.usersSubject.getValue();
       this.usersSubject.next([added, ...current]);
       this.loadingSubject.next(false);
